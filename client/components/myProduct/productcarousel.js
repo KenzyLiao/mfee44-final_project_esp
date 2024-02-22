035dc103b7c8f4ee3a5924257097ebccceb68f77
ice <ice@ice.gmail>
2024-02-22 22:03:33 +0800

    按鈕

--- client/components/myProduct/productcarousel.js
+++ client/components/myProduct/productcarousel.js
@@ -1,81 +1,54 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
-import 'swiper/css/free-mode'
 import 'swiper/css/navigation'
-import 'swiper/css/thumbs'
-import { Autoplay, FreeMode, Navigation, Thumbs } from 'swiper/modules'
-import Image from 'next/image'
-import styles from './productcarousel.module.scss'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+import SwiperCore, { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper'
 
-export default function Carousel() {
+// 初始化 Swiper 插件
+SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay])
+
+const ProductCarousel = () => {
+  // 设定轮播图的初始状态
   const [thumbsSwiper, setThumbsSwiper] = useState(null)
 
   return (
-    <div className={styles['product-carousel-container']}>
+    <div className="product-carousel-container">
       <Swiper
         style={{
           '--swiper-navigation-color': '#fff',
           '--swiper-pagination-color': '#fff',
         }}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
         spaceBetween={10}
         navigation={true}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[Autoplay, FreeMode, Navigation, Thumbs]}
-        className={`${styles.mySwiper2} ${styles['product__carousel']}`}
+        modules={[Autoplay, Navigation, Pagination, Scrollbar]}
+        className="mySwiper2 product__carousel"
       >
         <SwiperSlide>
-          <div className={styles['swiper-slide-image-container']}>
-            <Image
-              src="/images/product/slide/t1.jpg"
-              alt="Product 1"
-              width={800}
-              height={300}
-            />
+          <div className="swiper-slide-image-container">
+            <img src="/images/product/slide/t1.jpg" alt="Product 1" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className={styles['swiper-slide-image-container']}>
-            <Image
-              src="/images/product/slide/t2.jpg"
-              alt="Product 2"
-              width={800}
-              height={300}
-            />
+          <div className="swiper-slide-image-container">
+            <img src="/images/product/slide/t2.jpg" alt="Product 2" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className={styles['swiper-slide-image-container']}>
-            <Image
-              src="/images/product/slide/t3.jpg"
-              alt="Product 3"
-              width={800}
-              height={300}
-            />
+          <div className="swiper-slide-image-container">
+            <img src="/images/product/slide/t3.jpg" alt="Product 3" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className={styles['swiper-slide-image-container']}>
-            <Image
-              src="/images/product/slide/t4.jpg"
-              alt="Product 4"
-              width={800}
-              height={300}
-            />
+          <div className="swiper-slide-image-container">
+            <img src="/images/product/slide/t4.jpg" alt="Product 4" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className={styles['swiper-slide-image-container']}>
-            <Image
-              src="/images/product/slide/t5.jpg"
-              alt="Product 5"
-              width={800}
-              height={300}
-            />
+          <div className="swiper-slide-image-container">
+            <img src="/images/product/slide/t5.jpg" alt="Product 5" />
           </div>
         </SwiperSlide>
       </Swiper>
@@ -85,50 +58,27 @@ export default function Carousel() {
         slidesPerView={4}
         freeMode={true}
         watchSlidesProgress={true}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className={`${styles.mySwiper} ${styles['product__carousel']}`}
+        modules={[Scrollbar]}
+        className="mySwiper product__carousel"
       >
         <SwiperSlide>
-          <Image
-            src="/images/product/slide/t1.jpg"
-            alt="Thumbnail 1"
-            width={200}
-            height={150}
-          />
+          <img src="/images/product/slide/t1.jpg" alt="Thumbnail 1" />
         </SwiperSlide>
         <SwiperSlide>
-          <Image
-            src="/images/product/slide/t2.jpg"
-            alt="Thumbnail 2"
-            width={200}
-            height={150}
-          />
+          <img src="/images/product/slide/t2.jpg" alt="Thumbnail 2" />
         </SwiperSlide>
         <SwiperSlide>
-          <Image
-            src="/images/product/slide/t3.jpg"
-            alt="Thumbnail 3"
-            width={200}
-            height={150}
-          />
+          <img src="/images/product/slide/t3.jpg" alt="Thumbnail 3" />
         </SwiperSlide>
         <SwiperSlide>
-          <Image
-            src="/images/product/slide/t4.jpg"
-            alt="Thumbnail 4"
-            width={200}
-            height={150}
-          />
+          <img src="/images/product/slide/t4.jpg" alt="Thumbnail 4" />
         </SwiperSlide>
         <SwiperSlide>
-          <Image
-            src="/images/product/slide/t5.jpg"
-            alt="Thumbnail 5"
-            width={200}
-            height={150}
-          />
+          <img src="/images/product/slide/t5.jpg" alt="Thumbnail 5" />
         </SwiperSlide>
       </Swiper>
     </div>
   )
 }
+
+export default ProductCarousel
