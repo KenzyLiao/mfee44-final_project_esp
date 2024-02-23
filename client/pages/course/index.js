d616b29907ff8aa941702d0b9dee0f845080215b
hao <h85818@gmail.com>
2024-02-23 16:29:47 +0800

    更新格式

--- client/pages/course/index.js
+++ client/pages/course/index.js
@@ -4,26 +4,27 @@ import CardGroupTitle from '@/components/course/card-group-title.js'
 export default function CoursePage() {
   const data = [
     {
-      title:"我的課程",
-      subTitle:"新的一天持續學習"
+      title: '我的課程',
+      subTitle: '新的一天持續學習',
     },
     {
-      title:"興趣課程",
-      subTitle:"推薦您可能喜歡"
+      title: '興趣課程',
+      subTitle: '推薦您可能喜歡',
     },
     {
-      title:"熱門課程",
-      subTitle: "跟許多人一起學習"
-    }
+      title: '熱門課程',
+      subTitle: '跟許多人一起學習',
+    },
   ]
-  return (<>
-    <div>課程首頁</div>
-    {data.map((item, idx) => (
-      <div className='mb-5'>
-        <CardGroupTitle key={idx} {...item}/>
-        <CardGroup />
-      </div>
-    ))}
-    
-  </>)
+  return (
+    <>
+      <div>課程首頁</div>
+      {data.map((item, idx) => (
+        <div className="mb-5" key={idx}>
+          <CardGroupTitle {...item} />
+          <CardGroup />
+        </div>
+      ))}
+    </>
+  )
 }
--- client/pages/course/overview.js
+++ client/pages/course/overview.js
@@ -3,17 +3,14 @@ import CardGroup from '@/components/course/card-group.js'
 import FilterBar from '@/components/course/filter-bar'
 
 export default function CoursePage() {
-
   return (
-  <>
-    <FilterBar />
-    {
-      Array.from({ length: 5 }).map((_, i) => (
-        <div className='mb-5'>
-          <CardGroup key={i} />
+    <>
+      <FilterBar />
+      {Array.from({ length: 5 }).map((_, i) => (
+        <div className="mb-5" key={i}>
+          <CardGroup />
         </div>
-      ))
-    }
-    
-  </>)
+      ))}
+    </>
+  )
 }
