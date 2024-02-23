fe33ff5b8b878fb1b7a48843d48500366150709e
ianchen3236 <ianmars3236@gmail.com>
2024-02-23 16:33:16 +0800

    ian第一次上傳

---
+++ client/pages/coupon/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import TearStrip from '@/components/myCoupon/tearStrip'
+export default function Home() {
+  return (
+    <>
+      <p className="text-h3"></p>
+    </>
+  )
+}
