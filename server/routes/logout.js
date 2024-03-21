a2573bec8782f70b5234b7beddcbc8708a2fcb4e
Kenzy <kenzy@test.com>
2024-03-22 03:39:23 +0800

    完成上一次合併

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -3,15 +3,22 @@ import cookieParser from 'cookie-parser'
 
 const router = express.Router()
 
-// 使用 cookie-parser 中间件来解析 HTTP 请求中的 cookie
+// 使用 cookie-parser 中間件來解析 HTTP 請求中的 cookie
 router.use(cookieParser())
 
-// 创建处理登出请求的路由
+// 創建處理登出請求的路由
 router.post('/', (req, res) => {
-  // 清除名为 'authToken' 的 HttpOnly cookie
-  // 确保设置此 cookie 的选项与设置 token 时的选项一致，尤其是 path 和 domain
-  res.clearCookie('authToken', { path: '/' })
-  return res.status(200).json({ message: '您已成功登出。' })
+// 清除名為 'authToken' 的 HttpOnly cookie
+// 確保設置此 cookie 的選項與設置 token 時的選項一致，尤其是 path 和 domain
+res.clearCookie('authToken', { path: '/' })
+return res.status(200).json({ message: '您已成功登出。' })
 })
 
 export default router
+
+
+
+
+
+
+
